b3a4d76e3819e78a7abfb086670b43462f01e943
DhanaLakshmi Singitham <dhanalakshmis@caspex.com>
2024-11-27 19:07:54 +0530

    alphanumeric separation program

--- hands-on/alphanumeric.py
+++ hands-on/alphanumeric.py
@@ -14,29 +14,3 @@ def alphanumericseperation(str):
 
 str = "dha#na2la3kshmi4"
 alphanumericseperation(str)
-# a = str.isdigit()
-# b = str.isalpha()
-# # print(a, b)
-# for x in str:
-#     if type(x) == a:
-#         print(a)
-#     else:
-#         print(b)
-
-"""
-# str = list(a)
-str = ["a", 2, "b", 4]
-for x in str:
-    if type(x) == int:
-        continue
-    print(x, type(x))
-
-"""
-
-# input: abc#d
-# output: abd    
-
-
-
-
-  
